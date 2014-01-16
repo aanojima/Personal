bfb8ade0c73432d8cb62aa12eb9b6ab66495aaab
Aaron Nojima <aanojima@mit.edu>
2014-01-16 06:14:39 +0000

    Scroll Arrow

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	
+	var idleState = false;
+	var idleWait = 2000;
+	var idleTimer = null;
+
 	$(".edu-icon, .work-icon").click(function(){
 		$("#hidden").fadeIn(500,function(){
 			$(".main_content,#home,nav").one("click", function(){
@@ -6,7 +11,23 @@ $(document).ready(function(){
 			});
 		});
 	});
+
 	$("#social_network li a").hover(function(){
 		$(this).effect('bounce');
 	}, function(){});
+
+	$('*').bind('mousemove keydown scroll', function () {
+		clearTimeout(idleTimer);
+		if (idleState == true && $(window).scrollTop() < 800) {
+			// Reactivated event
+			$("#scroll_item").fadeOut(400);
+		}
+		idleState = false;
+		idleTimer = setTimeout(function () {
+			// Idle Event
+			idleState = true;
+			$("#scroll_item").fadeIn(400);
+		}, idleWait);
+	});
+
 });

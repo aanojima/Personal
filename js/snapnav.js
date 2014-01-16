fc941e80837b90651427c60954f12e997a09ee40
Aaron Nojima <aanojima@mit.edu>
2014-01-16 04:24:39 +0000

    Sexy

--- js/snapnav.js
+++ js/snapnav.js
@@ -8,18 +8,21 @@ $(document).ready(function() {
 			
 			// if yes, add "fixed" class to the <nav>
 			// add padding top to the #content (value is same as the height of the nav)
-			$('nav').addClass('fixed').css('top','0').next().css('padding-top','60px');
+			$('#navigation-container').addClass('fixed').css('top','0');
+			$('.center-div').css({'padding-top' : '60px'});
+			
 		} else {
 			
 			// when scroll up or less than aboveHeight, remove the "fixed" class, and the padding-top
-			$('nav').removeClass('fixed').next().css('padding-top','0');
+			$('#navigation-container').removeClass('fixed');
+			$('.center-div').css({'padding-top' : '0px'});
+
 		}
 	});
 
 	$("a").click(function(){
-		$('nav').addClass('fixed').css('top','0').next().css('padding-top','60px');
 		$('html, body').animate({
-			scrollTop: $( $.attr(this, 'href') ).offset().top
+			scrollTop: $(window).scrollTop() >= 800 ? $( $.attr(this, 'href') ).offset().top : $( $.attr(this, 'href') ).offset().top - 100,
 		}, 500);
 		return false;
 	});

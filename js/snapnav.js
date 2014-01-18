05912c7a6b8c0e6ee671ded2f8a38356fefccc42
Aaron Nojima <aanojima@mit.edu>
2014-01-18 10:30:51 +0000

    Layout Nav Fix

--- js/snapnav.js
+++ js/snapnav.js
@@ -2,27 +2,29 @@ $(document).ready(function() {
 	// when scroll
 
 	$(window).scroll(function(){
-		
+		console.log("SCROLL");
 		//if scrolled down more than the header's height
-		if ($(window).scrollTop() >= 800){
+		if ($(window).scrollTop() >= $("#home").height()){
 			
 			// if yes, add "fixed" class to the <nav>
 			// add padding top to the #content (value is same as the height of the nav)
 			$('#navigation-container').addClass('fixed').css('top','0');
-			$('.center-div').css({'padding-top' : '60px'});
+			$('#content').css({'padding-top' : '60px'});
 
 		} else {
 			
 			// when scroll up or less than aboveHeight, remove the "fixed" class, and the padding-top
 			$('#navigation-container').removeClass('fixed');
-			$('.center-div').css({'padding-top' : '0px'});
+			$('#content').css({'padding-top' : '0px'});
 
 		}
 	});
 
 	$("a").click(function(){
 		$('html, body').animate({
-			scrollTop: $(window).scrollTop() >= 800 ? $( $.attr(this, 'href') ).offset().top : $( $.attr(this, 'href') ).offset().top - 100,
+			scrollTop: $(window).scrollTop() >= $("#home").height() ? 
+				$( $.attr(this, 'href') ).offset().top : 
+				$( $.attr(this, 'href') ).offset().top,
 		}, 500);
 		return false;
 	});
